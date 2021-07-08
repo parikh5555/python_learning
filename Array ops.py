209f49b6ba2b5fef7b590a243c4cb17fcd0de387
Abhishek Parikh <parikh5555@gmail.com>
2021-07-08 23:27:50 +0530

    Update Array ops.py

--- Array ops.py
+++ Array ops.py
@@ -1,14 +1,14 @@
-  if com[i][0] == 'append':
+  if com[i][0] == 'append':   #append to last element
         myarr.append(int(com[i][1]))
-    elif com[i][0] == 'print':
+    elif com[i][0] == 'print':  #Print arr
         print (myarr)
-    elif com[i][0] == 'insert':
+    elif com[i][0] == 'insert':  # Insert element to specific location
         myarr.insert(int(com[i][1]),(int(com[i][2])))
-    elif com[i][0] == 'remove' :
+    elif com[i][0] == 'remove' : # Remove array from specific location
         myarr.remove(int(com[i][1]))
-    elif com[i][0] == 'pop':
+    elif com[i][0] == 'pop': #Remove last element
         myarr.pop()
-    elif com[i][0] == 'reverse':
+    elif com[i][0] == 'reverse': #Reverse the array
         myarr = list(reversed(myarr))
-    elif com[i][0] == 'sort':
+    elif com[i][0] == 'sort':  # Sort the array
         myarr.sort()
