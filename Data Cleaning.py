c28a42ad8d16ca2e15229fe7e1e6391c6209dc0b
Abhishek Parikh <parikh5555@gmail.com>
2021-07-18 11:47:18 +0530

    Check missing values

---
+++ Data Cleaning.py
@@ -0,0 +1,26 @@
+#Import the libraries
+
+import pandas as pd
+import numpy as np
+
+#Read CSV / Excel / SQL
+
+sf_permits = pd.read_csv("../input/building-permit-applications-data/Building_Permits.csv")
+
+# set seed for reproducibility
+np.random.seed(0)
+
+#Analyze how data frame looks like - First five columns
+sf_permits.head()
+
+#Check missing value
+missing_values_count = sf_permits.isnull().sum() 
+missing_values_count[0:10]  ## See how missing values looks
+
+# how many total missing values do we have?
+total_cells = np.product(sf_permits.shape)
+total_missing = missing_values_count.sum()
+
+# percent of data that is missing
+percent_missing = (total_missing/total_cells) * 100
+print(percent_missing)
